5022aa00f702f545bb8e4b34b8835b2e994c19aa
rivanildojr <rivanildo_junior7@hotmail.com>
2020-10-17 18:39:25 -0300

    finalizado o header

--- projeto-final/src/App.js
+++ projeto-final/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import "./App.css";
+
+import Header from "./components/Header";
+import Routes from "./routes";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Dogs Aplication</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
