56be876c7954519392c723063668c41e1fdf98bd
rivanildojr <rivanildo_junior7@hotmail.com>
2020-10-25 01:22:22 -0300

    criando os graficos com a lib victory na pagina de estatistica

--- projeto-final/src/components/UserStats/index.js
+++ projeto-final/src/components/UserStats/index.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import styles from "./UserStats.module.css";
 
+import UserStatsGraphs from "../UserStatsGraphs";
+
 import Head from "../../utils/Head";
+import Error from "../../utils/Error";
+import Loading from "../../utils/Loading";
+
+import useFetch from "../../hooks/useFetch";
+
+import { STATS_GET } from "../../service/api";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatística" />
-      UserStats
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    (async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    })();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatística" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
